3a7ca7c33a76504a6bdd4c19fdf7c8f11c68b685
Michel Brousse <mbweb47@gmail.com>
2019-11-03 17:59:18 +0100

    exo 5:  post-it jQuery OK

--- Exercices 31-10-2019  jQuery/5/script.js
+++ Exercices 31-10-2019  jQuery/5/script.js
@@ -1,28 +1,19 @@
-(function() {
-document.getElementById("postIt").style.visibility = "hidden";
+$(function () {
+    $("#postIt").hide();
+    $("#affiche").click(function () {
+        $("#postIt").html("Vous avez cliqué sur le bouton 'Affiche'! ");
+        $("#postIt").show();
+    });
+    $("#cache").click(function () {
+        $("#postIt").hide();
+    });
+    $("#survole1").hover(function () {
+        $("#postIt").html("C'est gentil de me survoler...");
+        $("#postIt").show();
+    });
+    $("#survole1").mouseleave(function () {
+        $("#postIt").hide();
+    });
 
-var btnaffiche = document.getElementById("affiche");
-    btnaffiche.addEventListener("click", function post_it() {
-        document.getElementById("postIt").innerHTML = "Vous avez cliqué sur le bouton 'Affiche'! " ;  
-        document.getElementById("postIt").style.visibility = "visible";
-    }
-    , false);
 
-var btncache = document.getElementById("cache");
-    btncache.addEventListener("click", function cache() {
-        document.getElementById("postIt").style.visibility = "hidden";
-    }
-    , false);
-
-var p = document.getElementById("survole1");
-    p.addEventListener("mouseover", function survole() {
-        document.getElementById("postIt").innerHTML = "Vous avez cliqué sur le bouton 'Affiche'! " ;  
-        document.getElementById("postIt").style.visibility = "visible";
-    }
-    , false);
-    p.addEventListener("mouseout", function survole_off() {
-        document.getElementById("postIt").style.visibility = "hidden";
-    }
-    , false);
-
-}())
+});
